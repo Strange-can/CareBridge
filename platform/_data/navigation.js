593931691e2de516590e42b974138fcb9e32b65f
omar-hossam <omarhossam50@proton.me>
2026-04-30 18:42:30 +0300

    feat: implement hero section with Lincoln's help, add dark/light themes, update nav/side bars

--- platform/_data/navigation.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  { name: "Need help", iconName: 'first-aid', url: "/need-help" },
-  { name: "Donate", iconName: 'donate', url: "/requests" },
-  { name: "Hospital Portal", iconName: 'hospital', url: "/login-hospital" },
-  { name: "Email Us", iconName: 'envelope', url: "/email-us" }
-];
